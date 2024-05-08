75b75820201868e9cf87c856d843fa8187ca8cbf
Sergio M <sergiomorenomartinez544@gmail.com>
2024-05-08 11:10:04 +0200

    Implemented GroupController + endpoints

---
+++ GroupService/Controller/GroupController.cs
@@ -0,0 +1,33 @@
+﻿using Domain.DTO.Group;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GroupService.Controller;
+
+[ApiController]
+[Route("group")]
+public class GroupController : ControllerBase {
+
+    [HttpGet("members")]
+    public IActionResult AllMembersFromGroup([FromBody] GroupMembersReq request) {
+        if(request.GroupId == 1) {
+            return Ok("yo mama says hi");
+        }
+        return BadRequest("No bueno, to test this endpoint OK result insert 1 for group id");
+    }
+    
+    [HttpPost]
+    public IActionResult Create([FromBody] PostGroup request) {
+        if(request.Name == "admin") {
+            return Ok("Group created");
+        }
+        return BadRequest("No bueno, to test this endpoint OK result insert 'admin' for name");
+    }
+    
+    [HttpPost()]
+    public IActionResult Join([FromBody] JoinGroupReq request) {
+        if(request.GroupId == 1 && request.UserId == 1) {
+            return Ok("User joined group");
+        }
+        return BadRequest("No bueno, to test this endpoint OK result insert 1 and 1 for both ids");
+    }
+}
