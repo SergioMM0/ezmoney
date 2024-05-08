789d444fc4ec494da61fb2f2443e25abaf61690f
Sergio M <sergiomorenomartinez544@gmail.com>
2024-05-08 10:24:19 +0200

    Added User service (#2)

---
+++ UserService/Controller/UserController.cs
@@ -0,0 +1,36 @@
+﻿using Domain.DTO.User;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UserService.Controller;
+
+[ApiController]
+[Route("[controller]")]
+public class UserController : ControllerBase {
+    /// <summary>
+    /// Attempts to log in a user, returning a 200 OK if successful, 400 Bad Request if not
+    /// Sends a petition to the UserRepository to check if the user exists through RabbitMQ
+    /// </summary>
+    /// <param name="request"></param>
+    /// <returns>{IActionResult}</returns>
+    [HttpPost]
+    public IActionResult Login([FromBody] LoginUserReq request) {
+        if(request.Name == "admin") {
+            return Ok("Login successful");
+        }
+        return BadRequest("No bueno");
+    }
+    
+    /// <summary>
+    /// Attempts to register a user in the system, returning a 200 OK if successful, 400 Bad Request if not
+    /// Sends a petition to the UserRepository through RabbitMQ
+    /// </summary>
+    /// <param name="request"></param>
+    /// <returns></returns>
+    [HttpPost]
+    public IActionResult Register([FromBody] RegisterUserReq request) {
+        if(request.Name == "admin") {
+            return Ok("Login successful");
+        }
+        return BadRequest("No bueno");
+    }
+}
