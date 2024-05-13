2fb0ae8aac93ef525cde7a10b1237ea860cd4afa
Ádám Lőrincz <90683098+Ladam0203@users.noreply.github.com>
2024-05-13 14:50:31 +0200

    Authenticated endpoints, refactored UserService

--- deployable/AuthService/Configuration/AuthServiceConfiguration.cs
+++ deployable/AuthService/Configuration/AuthServiceConfiguration.cs
@@ -3,5 +3,6 @@
 public class AuthServiceConfiguration {
     //This could be split up into multiple configuration classes in the future
     public string Key { get; set; }
-    public string UserServiceUrl { get; set; }
+    public string CreateUserUrl { get; set; }
+    public string GetUserByPhoneNumberUrl { get; set; }
 }
--- deployable/AuthService/Services/AuthService.cs
+++ deployable/AuthService/Services/AuthService.cs
@@ -14,9 +14,15 @@ public class AuthService {
     }
     
     public async Task<AuthenticationToken> Register(RegisterUserReq request) {
-        //1. Send HTTP request to user service to register (create) a user
+        // 1. Parse the RegisterUserReq to a CreateUserReq
+        var createUserReq = new CreateUserReq {
+            Name = request.Name,
+            PhoneNumber = request.PhoneNumber
+        };
+        
+        //2. Send HTTP request to user service to register (create) a user
         var client = new HttpClient();
-        var response = await client.PostAsJsonAsync("http://user-service:8080/user/register", request);
+        var response = await client.PostAsJsonAsync(_config.CreateUserUrl, createUserReq);
         if (!response.IsSuccessStatusCode) {
             throw new Exception("Error creating user. User service returned: " + response.StatusCode);
         }
@@ -24,7 +30,7 @@ public class AuthService {
         UserResponse user = await response.Content.ReadFromJsonAsync<UserResponse>();
         Console.WriteLine("user created: " + user.Id + " " + user.Name + " " + user.PhoneNumber);
         
-        //2. Create a token for the user (with user data)
+        //3. Create a token for the user (with user data)
         var token = _jwtTokenService.CreateToken(user);
         return token;
     }
@@ -32,7 +38,7 @@ public class AuthService {
     public async Task<AuthenticationToken> Login(LoginUserReq request) {
         //1. Send HTTP request to user service to login a user
         var client = new HttpClient();
-        var response = await client.PostAsJsonAsync("http://user-service:8080/user/login", request);
+        var response = await client.GetAsync(_config.GetUserByPhoneNumberUrl + request.PhoneNumber);
         if (!response.IsSuccessStatusCode) {
             throw new Exception("Error logging in. User service returned: " + response.StatusCode);
         }
