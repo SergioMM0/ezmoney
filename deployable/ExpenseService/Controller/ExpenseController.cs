45ada5e7b31fcc1eeb65e46688c9b4b054173841
Sergio M <sergiomorenomartinez544@gmail.com>
2024-05-15 10:26:59 +0200

    Added documentation

--- deployable/ExpenseService/Controller/ExpenseController.cs
+++ deployable/ExpenseService/Controller/ExpenseController.cs
@@ -17,7 +17,18 @@ public class ExpenseController : ControllerBase {
         _rpcClient = rpcClient;
     }
 
+    /// <summary>
+    /// Retrieves all the expenses from a user in a group. Returns an empty list if no expenses were found.
+    /// <br/>- Sends a request through RPC to:
+    /// <br/>- ExpenseRepository
+    /// </summary>
+    /// <param name="groupId"><see cref="int"/></param>
+    /// <param name="userId"><see cref="int"/></param>
+    /// <returns>A <see cref="IActionResult"/> containing an <see cref="List{T}"/> of <see cref="ExpenseResponse"/> objects.</returns>
     [HttpGet("{groupId}/user/{userId}")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<ExpenseResponse>))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof (string))]
     public async Task<ActionResult<List<ExpenseResponse>>> GetExpensesFromUser([FromRoute] int groupId, [FromRoute] int userId) {
         try {
             var request = new GetExpensesUserReq() {
@@ -33,7 +44,17 @@ public class ExpenseController : ControllerBase {
         }
     }
 
+    /// <summary>
+    /// Retrieves all the expenses from a group. Returns an empty list if no expenses were found.
+    /// <br/>- Sends a request through RPC to:
+    /// <br/>- ExpenseRepository
+    /// </summary>
+    /// <param name="groupId"><see cref="int"/></param>
+    /// <returns>A <see cref="IActionResult"/> containing an <see cref="List{T}"/> of <see cref="ExpenseResponse"/> objects.</returns>
     [HttpGet("{groupId}/expenses")]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<ExpenseResponse>))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof (string))]
     public async Task<ActionResult<List<ExpenseResponse>>> GetExpensesFromGroup([FromRoute] int groupId) {
         try {
             var request = new GetExpensesReq() {
@@ -48,7 +69,17 @@ public class ExpenseController : ControllerBase {
         }
     }
 
+    /// <summary>
+    /// Creates an expense.
+    /// <br/>- Sends a request through RPC to:
+    /// <br/>- ExpenseRepository
+    /// </summary>
+    /// <param name="request"><see cref="PostExpense"/></param>
+    /// <returns><see cref="ExpenseResponse"/></returns>
     [HttpPost]
+    [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(List<ExpenseResponse>))]
+    [ProducesResponseType(StatusCodes.Status400BadRequest, Type = typeof(string))]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError, Type = typeof (string))]
     public async Task<ActionResult<ExpenseResponse>> Create([FromBody] PostExpense request) {
         try {
             // Add datetime to the request
