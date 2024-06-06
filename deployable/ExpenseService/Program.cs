4fada3e471a1b19d0200ce7c622fbda99c40a412
Tawfik <90477695+TawfikAzza@users.noreply.github.com>
2024-06-06 12:41:08 +0200

    Group and Expense CircuitBreaker and fallback strategy

--- deployable/ExpenseService/Program.cs
+++ deployable/ExpenseService/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Messages.RPC;
 using Microsoft.Extensions.Options;
+using Polly;
+using Polly.Extensions.Http;
 using RPC;
 using RPC.RpcFactory;
 
@@ -10,6 +12,41 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
+
+// HERE we add the IHttpClientFactory
+builder.Services.AddHttpClient();
+builder.Services.AddHttpClient("ExpenseRepoHTTP", client => {
+    client.BaseAddress = new Uri("http://expense-repo:8080");
+});
+// HTTP Circuit breakers
+
+var retryPolicy = HttpPolicyExtensions
+    .HandleTransientHttpError()
+    .Or<RpcTimeoutException>()
+    .WaitAndRetryAsync(1, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)), onRetry: (outcome, timespan, retryAttempt, context) =>
+    {
+        Console.WriteLine($"Retrying... attempt {retryAttempt}");
+    });
+
+var circuitBreakerPolicy = HttpPolicyExtensions
+    .HandleTransientHttpError()
+    .Or<RpcTimeoutException>()
+    .CircuitBreakerAsync(1, TimeSpan.FromSeconds(30), onBreak: (outcome, timespan) =>
+    {
+        Console.WriteLine("Circuit breaker opened!");
+    }, onReset: () =>
+    {
+        Console.WriteLine("Circuit breaker reset!");
+    });
+
+var timeoutPolicy = Policy.TimeoutAsync<HttpResponseMessage>(TimeSpan.FromSeconds(30), (context, timeSpan, task) => {
+    return Task.FromException<HttpResponseMessage>(new RpcTimeoutException("Request timed out."));
+});
+
+// Wrapping the policies in the correct order: timeoutPolicy should be the outermost
+var policies = Policy.WrapAsync( retryPolicy, circuitBreakerPolicy,timeoutPolicy);
+builder.Services.AddSingleton<IAsyncPolicy<HttpResponseMessage>>(policies);
+
 // Configure topics
 builder.Services.AddSingleton<IConnectionFactoryProvider, RpcFactory>();
 builder.Services.Configure<Topics>(builder.Configuration.GetSection("RPCMessages"));
